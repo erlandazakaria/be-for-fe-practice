7f70b83c128a69261235196280b34bf0a8642123
Erlanda Zakaria <erlandazakaria@gmail.com>
2022-04-16 03:30:18 +0700

    Clean up

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { AppModule } from './module';
 import { HttpExceptionFilter } from './lib/exception-filter.lib';
 
 async function bootstrap() {
