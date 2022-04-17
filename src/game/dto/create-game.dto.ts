51d8104fac953016bc597a34ba5cec7c729d85d1
Erlanda Zakaria <erlandazakaria@gmail.com>
2022-04-17 14:40:20 +0700

    Add 20 Top Game Metacritic

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -14,4 +14,16 @@ export class CreateGameDto {
     @IsNotEmpty({message: "Rating tidak boleh kosong"})
     @IsNumberString(null, {message: "Format rating salah"})
     rating: number;
+
+    @IsNotEmpty({message: "Tahun tidak boleh kosong"})
+    @IsNumberString(null, {message: "Format tahun salah"})
+    year: number;
+
+    @IsNotEmpty({message: "Genre tidak boleh kosong"})
+    @IsString({message: "Format genre salah"})
+    genre: string;
+
+    @IsNotEmpty({message: "Platform tidak boleh kosong"})
+    @IsString({message: "Format platform salah"})
+    platform: string;
 }
